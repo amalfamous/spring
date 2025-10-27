c38b7735c33dee070e14fddb32048e1fea556b2b
amal <amalfms@gmail.com>
2025-10-27 22:00:23 +0100

    fini

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -17,16 +17,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	@Bean
-	CommandLineRunner start(CompteRepository compteRepository){
-		return args -> {
-			compteRepository.save(new Compte(null, Math.random()*9000, new Date(), TypeCompte.EPARGNE));
-			compteRepository.save(new Compte(null, Math.random()*9000, new Date(), TypeCompte.COURANT));
-			compteRepository.save(new Compte(null, Math.random()*9000, new Date(), TypeCompte.EPARGNE));
 
-			compteRepository.findAll().forEach(c -> {
-				System.out.println(c.toString());
-			});
-		};
-	}
 }
--- src/main/java/com/example/spring/entities/Compte.java
+++ src/main/java/com/example/spring/entities/Compte.java
@@ -1,6 +1,8 @@
 package com.example.spring.entities;
 
+import com.example.spring.entities.TypeCompte;
 import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,15 +13,18 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@XmlRootElement  // Permet la sérialisation XML (important pour JAX-RS)
 public class Compte {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private double solde;
 
     @Temporal(TemporalType.DATE)
     private Date dateCreation;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING) // Stocke la valeur textuelle de l'enum
     private TypeCompte type;
 }
